26dcf7a1d69c78795e20a2fc09a2658b28212cc0
Tommaso van Gelderen <tommasovangelderen@gmail.com>
2022-08-31 22:41:06 +0200

    SchrijfOp fix?

--- Kaart/ConsoleTekener.cs
+++ Kaart/ConsoleTekener.cs
@@ -3,7 +3,7 @@ class ConsoleTekener : Tekener {
 
     }
 
-    protected static void SchrijfOp(Coordinaat Positie, string Text) {
+    public void SchrijfOp(Coordinaat Positie, string Text) {
         if (Positie.x < 0 || Positie.y < 0)
         throw new Exception("Kan niet tekenen in het negatieve!");
         Console.SetCursorPosition(Positie.x, Positie.y);
