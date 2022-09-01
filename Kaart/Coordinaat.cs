a14b005773104046aeb94c5c9a94e6692b6325ae
Tommaso <tommasovangelderen@gmail.com>
2022-09-01 15:06:52 +0200

    Operator Coordinaat.cs

--- Kaart/Coordinaat.cs
+++ Kaart/Coordinaat.cs
@@ -8,4 +8,10 @@ struct Coordinaat {
         this.y = y;
     }
 
+    public static Coordinaat operator +(Coordinaat c1, Coordinaat c2) {
+        var x = c1.x + c2.x;
+        var y = c1.y + c2.y;
+        return new Coordinaat(x,y);
+    }
+
 }
